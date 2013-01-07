9508375f7462a4dd91e73f32d51bc792a8b917e2
Christian Mauceri <cmauceri@gmail.com>
2013-01-07 10:05:49 +0100

    Before pull matz 01072013

--- mrbgems/g/require/src/require.c
+++ mrbgems/g/require/src/require.c
@@ -9,10 +9,13 @@
 #include "mruby/irep.h"
 #include "mruby/proc.h"
 #include "mruby/compile.h"
+#include "mruby/dump.h"
 
 #include "opcode.h"
 
 #include <stdio.h>
+#include <dlfcn.h>
+
 //mrb_value mrb_mod_module_eval(mrb_state *mrb, mrb_value mod);
 mrb_value mrb_yield_internal(mrb_state *mrb, mrb_value b, int argc, mrb_value *argv, mrb_value self, struct RClass *c);
 
@@ -83,50 +86,58 @@ mrb_mod_module_eval(mrb_state *mrb, mrb_value mod)
     return mrb_yield_internal(mrb, b, 0, 0, mod, c);
 }
 
+mrb_value
+mrb_yield_internal(mrb_state *mrb, mrb_value b, int argc, mrb_value *argv, mrb_value self, struct RClass *c);
+
+static void
+replace_stop_with_return(mrb_state *mrb, mrb_irep *irep)
+{
+    if (irep->iseq[irep->ilen - 1] == MKOP_A(OP_STOP, 0)) {
+        irep->iseq = mrb_realloc(mrb, irep->iseq, (irep->ilen + 1) * sizeof(mrb_code));
+        irep->iseq[irep->ilen - 1] = MKOP_A(OP_LOADNIL, 0);
+        irep->iseq[irep->ilen] = MKOP_AB(OP_RETURN, 0, OP_R_NORMAL);
+        irep->ilen++;
+    }
+}
+
 static void
 load_file(mrb_state *mrb, mrb_value filename)
 {
-    FILE *fp;
-    mrb_value str, ret;
-    mrbc_context *c;
+    mrb_value str;
     int arena_idx;
+    int n;
+    void *dlh;
+    const char *data;
     
     arena_idx = mrb_gc_arena_save(mrb);
-    str = mrb_str_new(mrb, RSTRING_PTR(filename), RSTRING_LEN(filename));
-    c = mrbc_context_new(mrb);
-    c->capture_errors = 1;
-    c->no_exec = 1;
-    mrbc_filename(mrb, c, RSTRING_PTR(str));
+    str = mrb_str_new_cstr(mrb, "data_");
+    mrb_str_concat(mrb, str, mrb_str_new(mrb, RSTRING_PTR(filename), RSTRING_LEN(filename) - 3));
     
-    fp = fopen(c->filename, "r");
-    if (!fp) {
-        mrbc_context_free(mrb, c);
-        mrb_raisef(mrb, E_LOAD_ERROR, "file '%s' not found.", RSTRING_PTR(str));
+    dlh = dlopen(NULL, RTLD_LAZY);
+    data = (const char *)dlsym(dlh, RSTRING_PTR(str));
+    
+    if (!data) {
+        dlclose(dlh);
+        mrb_raisef(mrb, E_SCRIPT_ERROR, "file '%s' not found.", RSTRING_PTR(str));
     }
-    ret = mrb_load_file_cxt(mrb, fp, c);
-    fclose(fp);
-    mrbc_context_free(mrb, c);
+    n = mrb_read_irep(mrb, data);
+    dlclose(dlh);
+    
     mrb_gc_arena_restore(mrb, arena_idx);
     
-    if (mrb_fixnum_p(ret)) {
-        mrb_run_irep_as_proc(mrb, mrb->irep[mrb_fixnum(ret)]);
+    if (n >= 0) {
+        struct RProc *proc;
+        mrb_irep *irep = mrb->irep[n];
+        
+        replace_stop_with_return(mrb, irep);
+        proc = mrb_proc_new(mrb, irep);
+        proc->target_class = mrb->object_class;
+        mrb_yield_internal(mrb, mrb_obj_value(proc), 0, NULL, mrb_top_self(mrb), mrb->object_class);
     }
     else if (mrb->exc) {
         // fail to load.
         longjmp(*(jmp_buf*)mrb->jmp, 1);
     }
-}
-
-
-
-mrb_value
-mrb_f_load(mrb_state *mrb, mrb_value self)
-{
-    mrb_value filename;
-    
-    mrb_get_args(mrb, "S", &filename);
-    load_file(mrb, filename);
-    return mrb_true_value();
 }
 
 mrb_value
@@ -137,19 +148,8 @@ mrb_f_require(mrb_state *mrb, mrb_value self)
     mrb_sym sym_loaded_features, sym_loading_features;
     jmp_buf c_jmp;
     jmp_buf *prev_jmp;
-    mrb_value gv,path;
     
     mrb_get_args(mrb, "S", &filename);
-    /* If filename should be expanded to absolute path, modify it here. */
-    mrb_value argv[1];
-    
-    
-    gv = mrb_class_new_instance(mrb, 1, argv, mrb_class_get(mrb, "GlobalVars"));
-    
-    argv[0] = mrb_str_new2(mrb, "path");
-    path = mrb_funcall_argv(mrb, gv, mrb_intern(mrb, "get"), 1,argv);
-    
-    filename = mrb_str_append(mrb, path, filename);
     
     /* Check LOADED_FEATURES */
     sym_loaded_features = mrb_intern(mrb, "$LOADED_FEATURES");
@@ -159,7 +159,6 @@ mrb_f_require(mrb_state *mrb, mrb_value self)
         mrb_gv_set(mrb, sym_loaded_features, loaded_features);
     }
     len = RARRAY_LEN(loaded_features);
-    
     for (i=0; i<len; i++) {
         if (mrb_str_cmp(mrb, RARRAY_PTR(loaded_features)[i], filename) == 0) break;
     }
@@ -190,6 +189,16 @@ mrb_f_require(mrb_state *mrb, mrb_value self)
     mrb_hash_delete_key(mrb, loading_features, filename);
     mrb_ary_push(mrb, loaded_features, filename);
     
+    return mrb_true_value();
+}
+
+mrb_value
+mrb_f_load(mrb_state *mrb, mrb_value self)
+{
+    mrb_value filename;
+    
+    mrb_get_args(mrb, "S", &filename);
+    load_file(mrb, filename);
     return mrb_true_value();
 }
 
